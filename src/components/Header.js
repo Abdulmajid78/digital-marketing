18adc6a84ba7152693de3938a09ce514a5fc269a
Abdumajid7 <jackodilov@gmail.com>
2024-01-15 00:33:57 +0500

    done header

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <section className="header-section-cust">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xl-6 d-flex flex-column justify-content-center align-items-start">
+                            <div className="text-back">
+                                Digital Marketing
+                            </div>
+                            <h1>Приведите свой бизнес к новым высотам с нашей компанией!</h1>
+                            <p>Надежные решения для вашего успеха.</p>
+                            <button>Бесплатная Консультация</button>
+                        </div>
+                        <div className="col-xl-1"></div>
+                        <div className="col-xl-5">
+                            <div className="main-image"><img src="/assets/images/retangle2.png" alt=""/></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default Header;
